b9acf0a71e43ab4487cf217f96a443cc310c6cd0
the-future-warrior <saptakmanna17@gmail.com>
2022-12-31 08:00:21 +0530

    Login & Sign Up page UI added...

--- app/src/main/java/com/mr_17/evira/activity/LoginActivity.java
+++ app/src/main/java/com/mr_17/evira/activity/LoginActivity.java
@@ -1,16 +1,49 @@
 package com.mr_17.evira.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatTextView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.mr_17.evira.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private AppCompatTextView signUpLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        InitializeFields();
+
+        signUpLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendToActivity(SignUpActivity.class, true);
+            }
+        });
+    }
+
+    private void InitializeFields()
+    {
+        signUpLink = findViewById(R.id.sign_up_link);
+    }
+
+    private void SendToActivity(Class<? extends Activity> activityClass, boolean backEnabled)
+    {
+        Intent intent = new Intent(this, activityClass);
+
+        if (!backEnabled)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(intent);
+
+        if (!backEnabled)
+            finish();
     }
 }
--- app/src/main/java/com/mr_17/evira/activity/WelcomeActivity.java
+++ app/src/main/java/com/mr_17/evira/activity/WelcomeActivity.java
@@ -1,17 +1,51 @@
 package com.mr_17.evira.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.mr_17.evira.R;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private AppCompatButton userButton, adminButton, superAdminButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
+        InitializeFields();
+
+        userButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SendToActivity(LoginActivity.class, true);
+            }
+        });
+    }
+
+    private void InitializeFields()
+    {
+        userButton = findViewById(R.id.user_button);
+        adminButton = findViewById(R.id.admin_button);
+        superAdminButton = findViewById(R.id.super_admin_button);
+    }
+
+    private void SendToActivity(Class<? extends Activity> activityClass, boolean backEnabled)
+    {
+        Intent intent = new Intent(this, activityClass);
+
+        if (!backEnabled)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        startActivity(intent);
+
+        if (!backEnabled)
+            finish();
     }
 }
